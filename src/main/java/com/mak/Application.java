0997d61d1c50e864296c34fac1bf5f56ed7423f7
Maratib Ali Khan <maratib@gmail.com>
2023-05-04 18:41:35 +0500

    intial commit

---
+++ src/main/java/com/mak/Application.java
@@ -0,0 +1,35 @@
+package com.mak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.beans.factory.annotation.Value;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootApplication
+public class Application {
+
+	@Value("${server.port}")
+	private int serverPort;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@PostConstruct()
+	public void onStart() throws Exception {
+
+		// System.out.println("*** Application started at PORT : " + serverPort);
+		log.info("*** Application started at PORT : " + serverPort);
+	}
+
+	@PreDestroy
+	public void onExit() {
+		// System.out.println("*** Application stopped.");
+		log.info("*** Application stopped.");
+	}
+
+}
--- src/test/java/com/mak/ApplicationTests.java
+++ src/test/java/com/mak/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.mak.starter;
+package com.mak;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
