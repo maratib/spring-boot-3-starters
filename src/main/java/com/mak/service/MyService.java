67f40c3df81a73b8c67b090a2f6fcd83ba870c3e
Maratib Ali Khan <maratib@gmail.com>
2023-05-13 16:18:17 +0500

    Service test added

---
+++ src/main/java/com/mak/service/MyService.java
@@ -0,0 +1,11 @@
+package com.mak.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+
+    public void doTest() {
+        System.out.println("from doTest!");
+    }
+}
