f4533b57470f233e0e5f155cac735c21e7c9677f
Maratib Ali Khan <maratib@gmail.com>
2023-05-19 19:21:54 +0500

    Plain java testing

---
+++ src/test/java/com/mak/service/PlainServiceTest.java
@@ -0,0 +1,14 @@
+package com.mak.service;
+
+import org.junit.jupiter.api.Test;
+
+public class PlainServiceTest {
+
+    PlainService plainService = new PlainService();
+
+    @Test
+    public void testFoo() {
+        plainService.doTestFoo();
+
+    }
+}
